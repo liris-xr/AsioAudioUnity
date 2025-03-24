6befd6146268067bd5bc02f8642bb03e711ec39f
Eliott Zimmermann <37654999+eliottzim@users.noreply.github.com>
2025-03-24 10:59:21 +0100

    Fix string save bug when dragging AudioFile on CustomAsioAudioSource UI

--- Assets/AsioAudioUnity/Scripts/Editor/CustomAsioAudioSourceEditor.cs
+++ Assets/AsioAudioUnity/Scripts/Editor/CustomAsioAudioSourceEditor.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 namespace AsioAudioUnity
@@ -9,7 +10,7 @@ namespace AsioAudioUnity
     {
         public override void OnInspectorGUI()
         {
-            CustomAsioAudioSource myScript = (CustomAsioAudioSource)target;
+            serializedObject.Update();
 
             // Create a field in the editor to allow drag-and-drop of the audio file
             GUILayout.Label("Drag an audio file here");
@@ -18,30 +19,25 @@ namespace AsioAudioUnity
             Rect dropArea = GUILayoutUtility.GetRect(0f, 50f, GUILayout.ExpandWidth(true));
             GUI.Box(dropArea, "Put an audio file here");
 
-
+            // Check if a file has been dragged to this area
             if (dropArea.Contains(Event.current.mousePosition) && DragAndDrop.paths.Length > 0)
             {
-                // Check if a file has been dropped in this area
+                string filePath = DragAndDrop.paths[0];
+
                 if (Event.current.type == EventType.DragUpdated)
                 {
-                    DragAndDrop.AcceptDrag();
-                    Event.current.Use();
-
-
-                    string filePath = DragAndDrop.paths[0];
                     if (IsAudioFile(filePath)) DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
                 }
 
                 if (Event.current.type == EventType.DragExited)
                 {
-                    string filePath = DragAndDrop.paths[0];
-
                     // Check that the file is indeed an audio file
                     if (IsAudioFile(filePath))
                     {
                         string fileName = Path.GetFileName(filePath); // File name
                         string fileExtension = Path.GetExtension(filePath); // File extension
-                        myScript.AudioFilePath = filePath; // Store the full path in the script
+                        ((CustomAsioAudioSource)target).AudioFilePath = filePath; // Set the audio file path in the CustomAsioAudioSource component
+                        EditorUtility.SetDirty((CustomAsioAudioSource)target);
                         Debug.Log("File name: " + fileName);
                         Debug.Log("File extension: " + fileExtension);
                     }
@@ -52,7 +48,7 @@ namespace AsioAudioUnity
                 }
             }
 
-            
+            serializedObject.ApplyModifiedProperties();
 
             // Display the usual inspector
             DrawDefaultInspector();
