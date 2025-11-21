f64916e78d6bc36be79ea4f480c2c15ac18fa53b
Eliott Zimmermann <37654999+eliottzim@users.noreply.github.com>
2025-11-21 09:27:41 +0100

    Separate ReadOnlyAttribute and ReadOnlyDrawer

--- Assets/AsioAudioUnity/Scripts/Audio/AsioAudioManager.cs
+++ Assets/AsioAudioUnity/Scripts/Audio/AsioAudioManager.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 using NAudio.Wave;
-using NAudio.Wave.SampleProviders;
 
 namespace AsioAudioUnity
 {
--- Assets/AsioAudioUnity/Scripts/ReadOnlyAttribute.cs
+++ Assets/AsioAudioUnity/Scripts/ReadOnlyAttribute.cs
@@ -1,24 +1,6 @@
-using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
-[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-public class ReadOnlyDrawer : PropertyDrawer
-{
-    public override float GetPropertyHeight(SerializedProperty property,
-                                            GUIContent label)
-    {
-        return EditorGUI.GetPropertyHeight(property, label, true);
-    }
-
-    public override void OnGUI(Rect position,
-                                SerializedProperty property,
-                                GUIContent label)
-    {
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
-    }
-}
 
 public class ReadOnlyAttribute : PropertyAttribute
 {
